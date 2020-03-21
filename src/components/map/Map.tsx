223eab78cc23f316a59b1430559e076c2d25e8a5
makiri <kireew.martin@icloud.com>
2020-03-21 19:54:54 +0100

    new google maps package with polygon

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,21 +1,55 @@
-import React, { FC } from "react"
-import GoogleMapReact from "google-map-react"
+import React from "react"
+import { GoogleMap, withGoogleMap, Polygon } from "react-google-maps"
 import { useGeolocation } from "../../hooks/useGeolocation"
 
-export const Map: FC = () => {
+export const Map = withGoogleMap(() => {
     const userGeolocation = useGeolocation()
+
+    // Define the LatLng coordinates for the outer path.
+    const polygon = [
+        [
+            { lat: 12.364, lng: 53.207 }, // north west
+            { lat: 15.364, lng: 53.207 }, // south west
+            { lat: 15.364, lng: 58.207 }, // south east
+            { lat: 12.364, lng: 58.207 } // north east
+        ],
+        [
+            { lat: 13.364, lng: 54.207 },
+            { lat: 14.364, lng: 54.207 },
+            { lat: 14.364, lng: 55.207 },
+            { lat: 13.364, lng: 55.207 }
+        ],
+        [
+            { lat: 13.364, lng: 56.207 },
+            { lat: 14.364, lng: 56.207 },
+            { lat: 14.364, lng: 57.207 },
+            { lat: 13.364, lng: 57.207 }
+        ]
+    ]
+
     return (
         <div className="w-full h-full">
-            <GoogleMapReact
-                bootstrapURLKeys={{
-                    key: "AIzaSyC2hJYITb8r-BkWQil21x7KFT3wJe8NP5E"
-                }}
+            <GoogleMap
                 center={{
                     lat: userGeolocation.latitude,
                     lng: userGeolocation.longitude
                 }}
                 defaultZoom={11}
-            />
+            >
+                {polygon.map((path, index) => (
+                    <Polygon
+                        key={index}
+                        paths={path.map(({ lat, lng }) => ({ lng: lat, lat: lng }))}
+                        options={{
+                            fillColor: "#476a47",
+                            fillOpacity: 0.4,
+                            strokeColor: "#000",
+                            strokeOpacity: 1,
+                            strokeWeight: 1
+                        }}
+                    />
+                ))}
+            </GoogleMap>
         </div>
     )
-}
+})
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Map } from "../components/map/Map"
 export const Home: FC = () => {
     return (
         <div className="h-full rounded">
-            <Map />
+            <Map containerElement={<div className="h-full" />} mapElement={<div className="h-full" />} />
         </div>
     )
 }
